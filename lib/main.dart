21d458679da840308130b88bc6194517ad6d8f7f
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-09-18 14:25:42 -0300

    :building_construction: improve folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,21 @@
-import 'package:evita_ufg_app/main_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EvitaUFGApp extends StatelessWidget {
+  const EvitaUFGApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Evita UFG',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo With GetX'),
+      home: Container(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(MainController());
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Obx(
-              () => Text(
-                '${controller.count}',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: controller.increment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+void main() {
+  runApp(const EvitaUFGApp());
 }
