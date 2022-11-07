2e8abb3cf14867827d78b1502cf2d4471556c16d
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-11-07 20:22:45 -0300

    :bricks: improve api base url handling

--- lib/app/data/services/api.dart
+++ lib/app/data/services/api.dart
@@ -10,10 +10,11 @@ import 'package:evita_ufg_app/app/data/services/storage.dart';
 
 class ApiService {
   final _storageService = Get.find<StorageService>();
+  final String baseUrl = const String.fromEnvironment('API_URL');
 
   Dio getApi({bool needTokenAuth = false}) {
     final BaseOptions options = BaseOptions(
-      baseUrl: 'https://evita-ufg-api-staging.up.railway.app/api',
+      baseUrl: baseUrl,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
