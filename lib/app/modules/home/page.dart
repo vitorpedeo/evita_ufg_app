f9b36c421aa0c5626661ebbf79494bb0be85b444
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-11-26 19:57:03 -0300

    :sparkles: implement home drawer

--- lib/app/modules/home/page.dart
+++ lib/app/modules/home/page.dart
@@ -7,6 +7,7 @@ import 'package:shimmer/shimmer.dart';
 
 // Project imports:
 import 'package:evita_ufg_app/app/data/services/storage.dart';
+import 'package:evita_ufg_app/app/data/services/theme.dart';
 import 'package:evita_ufg_app/app/modules/home/controller.dart';
 import 'package:evita_ufg_app/app/widgets/app_card.dart';
 import 'package:evita_ufg_app/app/widgets/body_text.dart';
@@ -16,17 +17,135 @@ import 'package:evita_ufg_app/app/widgets/select_input.dart';
 import 'package:evita_ufg_app/app/widgets/text_input.dart';
 import 'package:evita_ufg_app/core/theme/custom.dart';
 import 'package:evita_ufg_app/core/theme/shimmer_colors.dart';
-import 'package:evita_ufg_app/core/utils/string_utils.dart';
 
 class HomePage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   final _controller = Get.find<HomeController>();
   final _storageService = Get.find<StorageService>();
+  final _themeService = Get.find<ThemeService>();
 
   HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        backgroundColor: context.theme.backgroundColor,
+        child: SafeArea(
+          child: ListView(
+            padding: const EdgeInsets.all(0),
+            children: [
+              Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: context.theme.textTheme.bodyText1!.color!,
+                    ),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    _storageService.user.value?.avatarUrl != null
+                        ? CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              _storageService.user.value!.avatarUrl!,
+                            ),
+                            minRadius: 28,
+                          )
+                        : const CircleAvatar(
+                            backgroundColor: CustomTheme.accentColor,
+                            minRadius: 28,
+                            child: Icon(
+                              Icons.person,
+                              color: CustomTheme.primaryColor,
+                            ),
+                          ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        HeadingText(
+                          _storageService.user.value?.name ?? '---',
+                          fontSize: 16,
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        BodyText(
+                          'Conta criada em ${_controller.formatAccountCreationDate(_storageService.user.value?.createdAt)}',
+                          fontSize: 12,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.account_circle,
+                  color: context.theme.textTheme.headline1?.color,
+                  size: 28,
+                ),
+                title: const BodyText(
+                  'Minha conta',
+                  fontWeight: FontWeight.w600,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 8,
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  _themeService.changeThemeMode();
+                },
+                leading: _themeService.isDarkMode.value
+                    ? Icon(
+                        Icons.nightlight_round,
+                        color: context.theme.textTheme.headline1?.color,
+                        size: 28,
+                      )
+                    : Icon(
+                        Icons.wb_sunny_rounded,
+                        color: context.theme.textTheme.headline1?.color,
+                        size: 28,
+                      ),
+                title: const BodyText(
+                  'Trocar tema atual',
+                  fontWeight: FontWeight.w600,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 8,
+                ),
+              ),
+              ListTile(
+                onTap: () {
+                  _controller.logout();
+                },
+                leading: Icon(
+                  Icons.logout_rounded,
+                  color: context.theme.textTheme.headline1?.color,
+                  size: 28,
+                ),
+                title: const BodyText(
+                  'Sair',
+                  fontWeight: FontWeight.w600,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 8,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       backgroundColor: context.theme.backgroundColor,
       body: SafeArea(
         child: Container(
@@ -264,37 +383,19 @@ class HomePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Obx(
-                    () => Row(
-                      children: [
-                        _storageService.user.value?.avatarUrl != null
-                            ? CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                  _storageService.user.value!.avatarUrl!,
-                                ),
-                              )
-                            : const CircleAvatar(
-                                backgroundColor: CustomTheme.accentColor,
-                                child: Icon(
-                                  Icons.person,
-                                  color: CustomTheme.primaryColor,
-                                ),
-                              ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        BodyText(
-                          'Olá, ',
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          _scaffoldKey.currentState!.openDrawer();
+                        },
+                        child: Icon(
+                          Icons.menu_rounded,
+                          size: 32,
                           color: context.theme.textTheme.headline1?.color,
                         ),
-                        BodyText(
-                          StringUtils.getFirstName(
-                              _storageService.user.value?.name),
-                          color: context.theme.textTheme.headline1?.color,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                   IconButton(
                     onPressed: () {
